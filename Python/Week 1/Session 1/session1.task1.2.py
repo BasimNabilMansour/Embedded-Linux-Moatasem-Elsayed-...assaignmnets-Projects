d8abb2674af75ca5a71fd2e9980a92e476ff9b96
BasimNabilMansour <basimnabilmansour@gmail.com>
2024-06-12 01:24:04 +0300

    Add files via upload

---
+++ Python/Week 1/Session 1/session1.task1.2.py
@@ -0,0 +1,7 @@
+vowel=['a','e','i','o','u','A','E','I','O','U']
+search=input()
+
+if search in vowel:
+    print("passed")
+else:
+    print("not passed")    
---
+++ Python/Week 1/Session 1/session1.task2.py
@@ -0,0 +1,5 @@
+radius=int(input("Enter the radius of the circle: "))
+
+area=3.14159*(radius**2)
+
+print("area=",area)
