97c46d1104c8e9c010bb36e02c44e2a64c6ecf70
BasimNabilMansour <basimnabilmansour@gmail.com>
2024-06-22 19:28:58 +0300

    Update session1.task1.1.py

--- Python/Week 1/Session 1/session1.task1.1.py
+++ Python/Week 1/Session 1/session1.task1.1.py
@@ -1,6 +1,7 @@
 while True:
+  list =[1,4,6,7,4,5,7,8,4,4,4,2,5]
+  search=input("enter the number your are searching for : ")
+  count=list.count(int(search))
+  print("the element",search,"occurs",count,"times")
 
-list =[1,4,6,7,4,5,7,8,4,4,4,2,5]
-search=input("enter the number your are searching for : ")
-count=list.count(int(search))
-print("the element",search,"occurs",count,"times")
+
