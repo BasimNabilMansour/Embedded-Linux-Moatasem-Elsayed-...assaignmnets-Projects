86795524a865f1ac00821d1db56949ea519fe2f6
BasimNabilMansour <basimnabilmansour@gmail.com>
2024-07-14 16:07:55 +0300

    Add files via upload

---
+++ C++/Cmake/First try/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "My name is Bassim" << std::endl;
+    std::cout << "This is a CMake file" << std::endl;
+    std::cout << "This is the last update "<< std::endl;
+    return 0;
+}
