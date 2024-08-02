ef3546d9300d9be66934a5cc71a39f872a58c860
BasimNabilMansour <basimnabilmansour@gmail.com>
2024-08-02 18:24:52 +0300

    Lec2.Task2.cpp

--- C++/Week2/Lec2.Task2.cpp
+++ C++/Week2/Lec2.Task2.cpp
@@ -1,43 +1,46 @@
 #include <iostream>
-
-
-void SearchNumber(int arr[], int size, int search) 
-{
-    int flag=0;
-    
-        for (int i = 0; i <= size; i++) 
-        {
-            if (arr[i] == search) 
-            {
-                std::cout << "The number " << search << " is found in the array in index."<<i << std::endl;
-                flag=1;
-            }
-            
-        }
-        if (flag==0)
-        {
-            std::cout << "The number " << search << " is not found in the array." << std::endl;
-            
-        }
-
-    
-}
+#include <vector>
+#include <algorithm>  
 
 int main() 
 {
-    while (1)
+    int size;
+
+    std::cout << "Enter the size of the array: ";
+    std::cin >> size;
+    //dynamic array
+    std::vector<int> array(size);
+
+    // Get the array elements from the user
+    std::cout << "Enter " << size << " elements of the array:\n";
+    for (int i = 0; i < size; i++)
     {
-        
-    
-    int arr[] = {1, 3, 5, 7, 2, 8, 6};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int num;
-
-    std::cout << "Enter the number to search: ";
-    std::cin >> num;
-
-    SearchNumber(arr,size,num);
+        std::cin >> array[i];
     }
+
+    // Output the vector elements to verify
+    std::cout << "The array elements are:\n";
+    for (int i = 0; i < size; i++) 
+    {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+
+    int target;
+    // Get the number to search for
+    std::cout << "Enter the number you want to search for: ";
+    std::cin >> target;
+
+    // Use std::find to search for the number in the vector
+    auto it = std::find(array.begin(), array.end(), target);
+
+    // Check if the number was found
+    if (it != array.end()) {
+        int index = std::distance(array.begin(), it);
+        std::cout << "Number "<<target<< " found at index " << index << std::endl;
+    } else {
+        std::cout << "Number not found in the array." << std::endl;
+    }
+
     return 0;
 }
-
